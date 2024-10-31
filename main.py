4968d97df8ac2a7dc597f44fc4379eeb65348832
Matheus Andrade Ribeiro de Moura Horácio <matheus.andrade5488@gmail.com>
2024-10-31 03:15:18 -0300

    atualizacao em 31/10/2024 03:15:18

--- main.py
+++ main.py
@@ -18,117 +18,129 @@ class APIHandler(BaseHTTPRequestHandler):
         if not self._authenticate():
             return
 
-        # Process the POST request for the `/api/speak` endpoint
+        # Determine the endpoint
         if self.path == '/api/speak':
-            content_length = int(self.headers['Content-Length'])
-            post_data = self.rfile.read(content_length)
-            data = json.loads(post_data)
-            morse=data.get("morse", False)
-            loop = data.get("loop", False)  # Check if loop is requested
-            phrase = data.get("phrase")
-            lang = data.get("lang", "pt")  # Default to Portuguese if no language is specified
-
-            # Handle loop requests
-            if loop:
-                response_messages = []
-                for i in range(1, 6):  # Adjust range for desired repetitions
-                    loop_phrase = f"Botafogo {i}, Peñarol 0"
-                    result = self.generate_and_play_audio(loop_phrase, lang)
-                    response_messages.append(result)
-
-                # Send combined results for loop
-                self.send_response(200)
-                self.send_header('Content-type', 'application/json')
-                self.end_headers()
-                self.wfile.write(json.dumps(response_messages).encode())
-            elif morse:
-                message=data.get("message")
-                subprocess.run('./script.sh ' + f'"{message}"', shell=True, capture_output=True, text=True)
-                self.send_response(200)
-                self.send_header('Content-type', 'application/json')
-                self.end_headers()
-                result = {'status': 'success', 'message': f'Código morse reproduzido: {message}'}
-                self.wfile.write(json.dumps(result, ensure_ascii=False).encode('utf-8'))
-            elif phrase:
-                # Handle single phrase request
-                result = self.generate_and_play_audio(phrase, lang)
-                self.send_response(200 if result['status'] == 'success' else 500)
-                self.send_header('Content-type', 'application/json')
-                self.end_headers()
-                self.wfile.write(json.dumps(result).encode())
-            elif self.path == '/api/alarm':
-                content_length = int(self.headers['Content-Length'])
-                post_data = self.rfile.read(content_length)
-                data = json.loads(post_data)
-
-                if data.get("alarm") and "time" in data:
-                    alarm_time = data["time"]
-                    try:
-                        # Parse HH_MM format
-                        alarm_hour, alarm_minute = map(int, alarm_time.split('_'))
-                        now = datetime.now()
-                        alarm_datetime = now.replace(hour=alarm_hour, minute=alarm_minute, second=0, microsecond=0)
-
-                        # If the time has already passed today, set it for tomorrow
-                        if alarm_datetime < now:
-                            alarm_datetime += timedelta(days=1)
-
-                        # Calculate the delay in seconds until the alarm time
-                        delay = (alarm_datetime - now).total_seconds()
-
-                        # Schedule ffplay to run at the specified time
-                        subprocess.Popen(["sleep", str(delay), "&&", "ffplay", "-nodisp", "-autoexit", 
-                                        "mixkit-retro-game-emergency-alarm-1000.wav"], shell=True)
-
-                        # Send response
-                        self.send_response(200)
-                        self.send_header('Content-type', 'application/json')
-                        self.end_headers()
-                        self.generate_and_play_audio(f"Alarme criado para {alarm_time} com sucesso! Bons sonhos.")
-                        self.wfile.write(json.dumps({"status": "success", "message": "Alarm set for " + alarm_time}).encode())
-                    except ValueError:
-                        # Invalid time format provided
-                        self.send_response(400)
-                        self.send_header('Content-type', 'application/json')
-                        self.end_headers()
-                        self.wfile.write(json.dumps({"status": "error", "message": "Invalid time format"}).encode())
-                else:
-                    # Alarm or time missing in the request
-                    self.send_response(400)
-                    self.send_header('Content-type', 'application/json')
-                    self.end_headers()
-                    self.wfile.write(json.dumps({"status": "error", "message": "Alarm flag or time missing"}).encode())
-            else:
-                # No phrase provided
-                self.send_response(400)
-                self.send_header('Content-type', 'application/json')
-                self.end_headers()
-                self.wfile.write(json.dumps({"status": "error", "message": "No phrase provided"}).encode())
-        
+            self.handle_speak_request()
+        elif self.path == '/api/alarm':
+            self.handle_alarm_request()
         else:
             # Invalid endpoint
             self.send_response(404)
             self.end_headers()
             self.wfile.write(b'Endpoint not found')
 
-    def run_script(self, script_path):
+    def handle_speak_request(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        data = json.loads(post_data)
+        morse = data.get("morse", False)
+        loop = data.get("loop", False)
+        phrase = data.get("phrase")
+        lang = data.get("lang", "pt")
+        pararAlarme=data.get("parar")
+
+        if loop:
+            response_messages = []
+            for i in range(1, 6):
+                loop_phrase = f"Botafogo {i}, Peñarol 0"
+                result = self.generate_and_play_audio(loop_phrase, lang)
+                response_messages.append(result)
+
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(response_messages).encode())
+        elif pararAlarme:
+            subprocess.run("killall ffplay", shell=True)
+            self.generate_and_play_audio("Alarme desativado com sucesso.", "pt")
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps({"status":"sucesso", "message": "alarme desativado!"}).encode())
+        elif morse:
+            message = data.get("message")
+            subprocess.run('./script.sh ' + f'"{message}"', shell=True, capture_output=True, text=True)
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            result = {'status': 'success', 'message': f'Código morse reproduzido: {message}'}
+            self.wfile.write(json.dumps(result, ensure_ascii=False).encode('utf-8'))
+        elif phrase:
+            result = self.generate_and_play_audio(phrase, lang)
+            self.send_response(200 if result['status'] == 'success' else 500)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(result).encode())
+        else:
+            self.send_response(400)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps({"status": "error", "message": "No phrase provided"}).encode())
+
+    def format_alarm_time_to_pt(self, alarm_time):
+    # Parse alarm_time in HH_MM format
         try:
-            output = subprocess.check_output(['bash', script_path], stderr=subprocess.STDOUT)
-            return {'status': 'success', 'output': output.decode()}
-        except subprocess.CalledProcessError as e:
-            return {'status': 'error', 'output': e.output.decode()}
+            hour, minute = map(int, alarm_time.split('_'))
+            alarm_dt = datetime.now().replace(hour=hour, minute=minute, second=0, microsecond=0)
+            
+            # Format in Portuguese
+            day_part = "da manhã" if hour < 12 else "da tarde" if hour < 18 else "da noite"
+            time_str = alarm_dt.strftime(f"%H horas e %M minutos {day_part}")
+            
+            return time_str
+        except ValueError:
+            return "Formato de hora inválido"
+    def handle_alarm_request(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        data = json.loads(post_data)
+
+        if data.get("alarm") and "time" in data:
+            alarm_time = data["time"]
+            try:
+                # Parse HH_MM format
+                alarm_hour, alarm_minute = map(int, alarm_time.split('_'))
+                now = datetime.now()
+                alarm_datetime = now.replace(hour=alarm_hour, minute=alarm_minute, second=0, microsecond=0)
+
+                # If the time has already passed today, set it for tomorrow
+                if alarm_datetime < now:
+                    alarm_datetime += timedelta(days=1)
+
+                # Calculate the delay in seconds until the alarm time
+                delay = (alarm_datetime - now).total_seconds()
+
+                # Schedule ffplay to run at the specified time
+                subprocess.run(f"bash -c 'sleep {int(delay)}; ffplay -nodisp -autoexit -loop 0 mixkit-retro-game-emergency-alarm-1000.wav' &", shell=True)
+
+
+                # Send response
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+                self.generate_and_play_audio(f"Alarme criado com sucesso para {self.format_alarm_time_to_pt(alarm_time)}! Bons sonhos.", "pt")
+                self.wfile.write(json.dumps({"status": "success", "message": f"Alarm set for {alarm_time}"}).encode())
+            except ValueError:
+                # Invalid time format provided
+                self.send_response(400)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+                self.wfile.write(json.dumps({"status": "error", "message": "Invalid time format"}).encode())
+        else:
+            # Alarm or time missing in the request
+            self.send_response(400)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps({"status": "error", "message": "Alarm flag or time missing"}).encode())
 
     def generate_and_play_audio(self, phrase, lang):
         try:
-            # Generate audio from phrase in the specified language
             tts = gTTS(phrase, lang=lang)
             audio_file = f"phrase_{phrase}.mp3"
             tts.save(audio_file)
 
-            # Play the generated audio file
             subprocess.run(['ffplay', '-nodisp', '-autoexit', audio_file])
 
-            # Delete the audio file after playing it
             if os.path.exists(audio_file):
                 os.remove(audio_file)
 
